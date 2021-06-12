8794bee6e79063f9adaa526906a0e3f4048e163c
Mona Lisa <sam.roberson32@gmail.com>
2021-06-11 21:49:16 -0600

    created homroutes and api routes

---
+++ controller/api/workoutRoutes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+
+const { Workout } = require("../../models");
+
+router.get("/", (req, res) => {
+  console.log("get request");
+  Workout.aggregate()
+    .addFields({
+      totalDuration: { $sum: "$exercises.duration" },
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  ).then((data) => {
+    res.json(data);
+  });
+});
+
+
+router.post("/", (req, res) => {
+  Workout.create(req.body).then((data) => {
+    res.json(data);
+  });
+});
+
+router.get("/range", (req, res) => {
+  Workout.aggregate()
+    .addFields({
+      totalDuration: { $sum: "$exercises.duration" },
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+module.exports = router;
---
+++ controller/homeRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+//path is to allow static file retrieval
+const path = require("path");
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+module.exports = router;
